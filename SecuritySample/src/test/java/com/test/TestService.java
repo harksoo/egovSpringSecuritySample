0c4e7c3af259876234a94e70794a1dc04bc0ec8e
harksoo <harksoo1974@gmail.com>
2014-07-04 10:59:52 +0900

    add TestId

--- SecuritySample/src/test/java/com/test/TestService.java
+++ SecuritySample/src/test/java/com/test/TestService.java
@@ -2,4 +2,14 @@ package com.test;
 
 public class TestService {
 	String testId = "";
+
+	public String getTestId() {
+		return testId;
+	}
+
+	public void setTestId(String testId) {
+		this.testId = testId;
+	}
+	
+	
 }
